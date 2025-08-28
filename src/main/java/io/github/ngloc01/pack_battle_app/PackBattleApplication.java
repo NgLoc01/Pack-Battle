9d22d517bfe755415cfb6904fe0b8b4bb6fdcab6
ngloc01 <NgLoc01@gmail.com>
2025-08-28 14:48:08 +0200

    First commit with spring Initializr

---
+++ src/main/java/io/github/ngloc01/pack_battle_app/PackBattleApplication.java
@@ -0,0 +1,13 @@
+package io.github.ngloc01.pack_battle_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ngloc01/pack_battle_app/PackBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ngloc01.pack_battle_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
